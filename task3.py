847274e5a96938042280c2c985b809989d3d1549
IaOksana <iashchuk.oksana@gmail.com>
2025-01-30 00:33:28 +0400

    task3 not done

--- task3.py
+++ task3.py
@@ -1,3 +1,5 @@
+# TO BE DONE lately
+
 # Завдання 3 (необов'язкове завдання). Ханойські башти
 # Напишіть програму, яка виконує переміщення дисків з стрижня А на стрижень С, використовуючи стрижень В як 
 # допоміжний. Диски мають різний розмір і розміщені на початковому стрижні у порядку зменшення розміру зверху 
@@ -29,29 +31,3 @@
 # Перемістити диск з A на C: 1
 # Проміжний стан: {'A': [], 'B': [], 'C': [3, 2, 1]}
 # Кінцевий стан: {'A': [], 'B': [], 'C': [3, 2, 1]}
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-
-def draw_triangle(vertices, ax):
-    triangle = patches.Polygon(vertices, fill=False, edgecolor='black')
-    ax.add_patch(triangle)
-
-def midpoint(point1, point2):
-    return [(point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2]
-
-def sierpinski(vertices, level, ax):
-    draw_triangle(vertices, ax)
-    if level > 0:
-        sierpinski([vertices[0], midpoint(vertices[0], vertices[1]), midpoint(vertices[0], vertices[2])], level-1, ax)
-        sierpinski([vertices[1], midpoint(vertices[0], vertices[1]), midpoint(vertices[1], vertices[2])], level-1, ax)
-        sierpinski([vertices[2], midpoint(vertices[2], vertices[1]), midpoint(vertices[0], vertices[2])], level-1, ax)
-
-def main():
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
-    ax.set_axis_off()
-    vertices = [[0, 0], [0.5, 0.75], [1, 0]]
-    sierpinski(vertices, 3, ax)
-    plt.show()
-
-main()
